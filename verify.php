4aecfaf4e97a4099644abfd0db694ce2cadb2f10
swmkook <49269612+swmkook@users.noreply.github.com>
2019-10-22 04:54:57 +0700

    remove token place message "YOUR TOKEN"

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'YOUR TOKEN';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
